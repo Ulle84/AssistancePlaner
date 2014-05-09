0e96b66891fde7c3d12eb5270b172290882c8560
Ulrich Belitz <u.belitz@icloud.com>
2014-05-09 16:06:00 +0200

    Login Feedback improved

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -11,9 +11,13 @@ function login() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             if (xmlhttp.responseText == "changePassword") {
                 window.location.href = "../PHP/changePassword.php";
+                response.innerHTML = "Login erfolgreich - Bitte Password wechseln!"
+                return;
             }
             if (xmlhttp.responseText == "OK") {
                 window.location.href = "../PHP/index.php";
+                response.innerHTML = "Login erfolgreich!"
+                return;
             }
 
             response.innerHTML = xmlhttp.responseText;
