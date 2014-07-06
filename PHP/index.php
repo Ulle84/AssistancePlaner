5676e0aa1212d12a03f1fca952d82cf7e4a9c209
Ulrich Belitz <u.belitz@icloud.com>
2014-07-06 08:10:44 +0200

    Docu update

--- PHP/index.php
+++ PHP/index.php
@@ -3,14 +3,25 @@
 <meta charset="utf-8">
 <html>
 <head>
-    <title>Assistenz Planer</title>
+    <title>Assistenzplaner</title>
     <link rel="stylesheet" type="text/css" href="../CSS/global.css" media="all"/>
 </head>
 <body>
 <?php include('navigation.php'); ?>
 
-<h1>Willkommen beim Assistenz Planer</h1>
-Bitte <a href="login.php">melden Sie sich an</a> oder <a href="createNewAccount.php">legen Sie einen neuen Klienten an</a>.
+<div class="wrapLongText">
+    <h1>Willkommen beim Assistenzplaner</h1>
+    Der Assistenzplaner ist ein Hilfsmittel für körperlich behinderte Menschen, die auf Assistenz angewiesen sind. <br/>
 
+    Mit Hilfe des Assistenzplaners ist es unter anderem möglich:
+    <ul>
+        <li>Assistenz-Teams zu verwalten</li>
+        <li>Dienstpläne zu erstellen</li>
+        <li>Aufgaben der Assistenten zu verwalten</li>
+    </ul>
+
+    Bitte <a href="login.php">melden Sie sich an</a> oder <a href="createNewAccount.php">legen Sie einen neuen Klienten
+        an</a>.
+</div>
 </body>
 </html>
